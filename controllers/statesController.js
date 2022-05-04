429aa064d411c676f40dbbbf338ef3000bb412ea
AndyRiedel <das_stig@yahoo.com>
2022-05-03 23:13:31 -0500

    added delete functionality for funfacts

--- controllers/statesController.js
+++ controllers/statesController.js
@@ -229,7 +229,6 @@ const addNewFact = async (req, res) => {
         indexParam = indexParam - 1;//adjust index
         thisState.funfacts[indexParam] = factParam;//update array
         const result = await thisState.save();//save the updated array
-        console.log(result);
         return res.json(result);
     }
     catch (err) {
@@ -239,9 +238,39 @@ const addNewFact = async (req, res) => {
 }
 
 const deleteFact = async (req, res) => {
-    if (!req?.body?.index){
+    let stateParam = req.params.state.toUpperCase();
+    if (!validState(stateParam)){//if no valid state abbrev. param, return invalid state message
+        return res.json({"message": "Invalid state abbreviation parameter"});
+    }
+    if (!req?.body?.index || req?.body?.index < 1){//if no index value passed or index == 0
         return res.status(400).json({'message': 'State fun fact index value required'});
-    }    
+    }
+    let indexParam = req?.body?.index;
+    try {
+        const thisState = await State.findOne({stateCode: stateParam}).exec();
+        if (indexParam > thisState.funfacts.length){//out of bounds index passed
+            let states = await getStates();
+            states = states.filter(st => st.code == stateParam);
+            return res.json( {
+                "message": "No Fun Fact found at that index for " + states[0].state
+            });
+        }
+        //in bounds index, update the funfacts array
+        indexParam = indexParam - 1;//adjust index
+        let newFacts = [];
+        let oldFacts = thisState.funfacts;
+        for (let i = 0; i < oldFacts.length; i++){
+            if (i != indexParam){
+                newFacts.push(oldFacts[i]);
+            }
+        }
+        thisState.funfacts = newFacts;//update array
+        const result = await thisState.save();//save the updated array
+        return res.json(result);
+    }
+    catch (err) {
+        console.error(err);
+    }
 }
 
 module.exports = {
