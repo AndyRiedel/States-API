c6157756edc0382177e56aecafadd8d313583338
AndyRiedel <das_stig@yahoo.com>
2022-05-04 19:46:37 -0500

    update cors

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 //enable cross origin requests
-const whitelist = ['https://www.google.com', 'http://127.0.0.1:5500', 'http://localhost:3500']
+const whitelist = ['https://dazzling-snickerdoodle-777101.netlify.app', 'http://127.0.0.1:5500', 'http://localhost:3500']
 const corsOptions = {
     origin: (origin, callback) => {
         if (whitelist.indexOf(origin) !== -1 || !origin){
